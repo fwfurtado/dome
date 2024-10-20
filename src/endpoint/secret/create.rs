9acee565974997daa7b38c70656b219902ce521b
Mimi <fwfurtado@gmail.com>
2024-10-20 02:11:05 -0300

    test: add api test

--- src/endpoint/secret/create.rs
+++ src/endpoint/secret/create.rs
@@ -4,14 +4,14 @@ use axum::response::IntoResponse;
 use axum::Json;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct CreateSecretRequest {
     name: String,
     plain_text: String,
     url: String,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct CreateSecretResponse {
     name: String,
     encrypted_text: String,
@@ -39,9 +39,68 @@ pub async fn create(Json(input): Json<CreateSecretRequest>) -> impl IntoResponse
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::endpoint::secret::routes;
+    use crate::routes;
+    use axum::{
+        body::Body,
+        http,
+        http::{Request, StatusCode},
+        Router,
+    };
+    use fake::{
+        faker::{company::en::CompanyName, internet::en::Password},
+        Fake, Faker,
+    };
+    use std::sync::LazyLock;
+
+    use http_body_util::BodyExt;
+
+    use tower::ServiceExt;
+
+    fn url() -> url::Url {
+        Faker.fake::<url::Url>()
+    }
+
+    static ROUTES: LazyLock<Router> = LazyLock::new(|| {
+        routes![
+            "/" => routes()
+        ]
+    });
 
     #[tokio::test]
-    async fn create_secret_test() {
-        // let router = Server::default().routes();
+    async fn should_create_a_secret() {
+        let name: String = CompanyName().fake();
+
+        let request_body = CreateSecretRequest {
+            name: name.clone(),
+            plain_text: Password(8..12).fake(),
+            url: url().to_string(),
+        };
+
+        let body_json = serde_json::to_string(&request_body).unwrap();
+
+        let response = ROUTES
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri("/")
+                    .method(http::Method::POST)
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .body(Body::from(body_json))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::CREATED);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+
+        let body = String::from_utf8(body.to_vec()).unwrap();
+
+        let secret: CreateSecretResponse = serde_json::from_str(&body).unwrap();
+
+        assert_eq!(secret.name, name);
+        assert!(!secret.encrypted_text.is_empty());
     }
 }
--- src/http/macros.rs
+++ src/http/macros.rs
@@ -3,7 +3,7 @@ macro_rules! routes {
     ($($prefix:expr => $routes:expr),*) => {
         {
             use axum::Router;
-            let mut router = Router::new();
+            let router = Router::new();
             $(
                 let router = router.nest($prefix, $routes);
             )*
